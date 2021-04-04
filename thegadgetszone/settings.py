eec002aa7fcd9fd053397b513c82c8aafafa9466
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-04-04 17:56:03 +0530

    done with aws cloud

--- thegadgetszone/settings.py
+++ thegadgetszone/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'thegadgetszone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'ecomdb',
+         'USER': 'chinmay101',
+         'PASSWORD': 'chinmay10',
+         'HOST': 'database-1.chvezftc7zlu.us-east-2.rds.amazonaws.com',
+         'PORT': '5432'
     }
 }
 
